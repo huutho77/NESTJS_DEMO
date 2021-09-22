3b8edf732b9b0978aaa8091962edcb5dff321a90
Huu Tho Nguyen <huutho1999@gmail.com>
2021-09-22 20:14:03 +0700

    Add .env file and re-config connection to db by .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(),
     ProductsModule,
     UsersModule,
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT;
+
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
 
   app.setGlobalPrefix('api');
   
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
