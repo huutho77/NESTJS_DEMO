258805982a19560cfd771dbb5e879cb70792ed40
Huu Tho Nguyen <huutho1999@gmail.com>
2021-10-06 21:30:57 +0700

    Update products.controller.ts

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Param, Delete, Put, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateProductDTO } from '../dto/product-create.dto';
 import { UpdateProductDTO } from '../dto/product-update.dto';
@@ -8,30 +8,30 @@ import { ProductsService } from './products.service';
 export class ProductsController {
   constructor(private productService: ProductsService) { }
 
-  @UseGuards(AuthGuard('jwt'))
+  // @UseGuards(AuthGuard('jwt'))
   @Get()
   async getAllProducts() {
     return await this.productService.findAllProducts();
   }
 
   @Get(':id')
-  getProductById(@Param('id') id: string) {
-    let product = this.productService.findProductById(id);
+  async getProductById(@Param('id') id: string) {
+    let product = await this.productService.findProductById(id);
     return product;
   }
 
   @Post('create')
-  createNewProduct(@Body() body: CreateProductDTO) {
-    return this.productService.createNewProduct(body);
+  async createNewProduct(@Body() body: CreateProductDTO) {
+    return await this.productService.createNewProduct(body);
   }
 
   @Put('update/:id')
-  updateProduct(@Param('id') id: string, @Body() body: UpdateProductDTO) {
-    return this.productService.updateProduct(id, body);
+  async updateProduct(@Param('id') id: string, @Body() body: UpdateProductDTO) {
+    return await this.productService.updateProduct(id, body);
   }
 
   @Delete('delete/:id')
-  deleteItem(@Param('id') id: string) {
-    return this.productService.deleteProduct(id);
+  async deleteItem(@Param('id') id: string) {
+    return await this.productService.deleteProduct(id);
   }
 }
